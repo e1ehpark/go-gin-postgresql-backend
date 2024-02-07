bbf1621c4966ef83ad313854c2def5b28bb69af2
e1ehpark <ehpark@enterprise1.co.kr>
2024-02-07 16:43:24 +0900

    api key 변수 선언  누락하여 추가

--- src/middlewares/AuthMiddleware.go
+++ src/middlewares/AuthMiddleware.go
@@ -6,6 +6,7 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		apiKey := c.GetHeader("X-API-Key")
 		if apiKey == "" {
 			c.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized to perform request. Please get a valid API key"})
 			return

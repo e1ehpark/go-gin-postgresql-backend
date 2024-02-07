45a35d2381d5584783a5de5cb28223c53c61a717
e1ehpark <ehpark@enterprise1.co.kr>
2024-02-07 17:06:30 +0900

    routers import 추가

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/e1ehpark/go-gin-postgresql-backend/src/middlewares"
 	"github.com/e1ehpark/go-gin-postgresql-backend/src/utils"
 	"github.com/e1ehpark/go-gin-postgresql-backend/src/models"
+	"github.com/e1ehpark/go-gin-postgresql-backend/src/routes"
 )
 
 func main() {
